e2117d8adc4129347f75f1401a0fa8af8d51f42a
Jake Hassel <jakehasssel@gmail.com>
2018-04-03 22:58:16 -0400

    Added avatar to card

--- src/components/RedditPost.jsx
+++ src/components/RedditPost.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import reddit_icon from '../reddit_icon.png';
+import link_icon from '../link_icon.png';
 
 const RedditPost = props => {
     let post = props.info;
+    let thumb = post.thumbnail;
+    if (thumb === 'self') {
+        thumb = reddit_icon;
+    } else if (thumb === 'default') {
+        thumb = link_icon;
+    }
     return (
         <Card className="post-card">
             <CardHeader
@@ -11,6 +19,7 @@ const RedditPost = props => {
                 subtitle={post.author}
                 actAsExpander={post.is_self}
                 showExpandableButton={post.is_self}
+                avatar={thumb}
             />
 
             <CardText expandable={post.is_self}>
