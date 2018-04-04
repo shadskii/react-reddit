601837e201265670742069d678b8a2f82e2a0b90
Jake Hassel <jakehasssel@gmail.com>
2018-04-03 22:30:36 -0400

    Move to reddit feed

--- src/components/App.js
+++ src/components/App.js
@@ -5,56 +5,20 @@ import '../styles/App.css';
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import RedditPost from './RedditPost';
+import RedditFeed from './RedditFeed';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      lastPostName: ''
-    };
-  }
-
-  fetch(url) {
-    var that = this;
-    if (url) {
-      fetch('https://www.reddit.com/r/' + url + '.json' + '?count=' + 25 + '&after=' + this.state.lastPostName)
-        .then((response) => response.json())
-        .then((result) => {
-          that.setState({
-            posts: result.data.children,
-            lastPostName: result.data.children[result.data.children.length - 1].data.name
-          });
-        });
-    }
-  }
-  componentWillMount() {
-    this.fetch("reactjs");
-  }
-
-
-  render() {
-    return (
-      <MuiThemeProvider>
-        <div className="App">
-          <AppBar
-            title={"Reactjs Reddit"}
-            showMenuIconButton={false}
-            iconElementRight={<FlatButton onClick={() => this.fetch('reactjs')} label="next" />
-            }
-          />
-          <div className="container">
-            {this.state.posts.map(function (el, index) {
-              return <RedditPost info={el.data} key={index} />
-            })}
-            <FlatButton onClick={() => this.fetch('reactjs')} label="more" />
-          </div>
-        </div>
-
-      </MuiThemeProvider >
-
-    );
-  }
-}
+const App = (props) => (
+  <MuiThemeProvider>
+    <div className="App">
+      <AppBar
+        title={"Reactjs Reddit"}
+        showMenuIconButton={false}
+        iconElementRight={<FlatButton onClick={() => this.fetch('reactjs')} label="next" />
+        }
+      />
+      <RedditFeed />
+    </div>
+  </MuiThemeProvider >
+);
 
 export default App;
---
+++ src/components/RedditFeed.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import RedditPost from './RedditPost';
+
+class RedditFeed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+            lastPostName: ''
+        };
+    }
+    fetch(url) {
+        var that = this;
+        if (url) {
+            fetch('https://www.reddit.com/r/' + url + '.json' + '?count=' + 25 + '&after=' + this.state.lastPostName)
+                .then((response) => response.json())
+                .then((result) => {
+                    that.setState({
+                        posts: result.data.children,
+                        lastPostName: result.data.children[result.data.children.length - 1].data.name
+                    });
+                });
+        }
+    }
+    componentWillMount() {
+        this.fetch("reactjs");
+    }
+    render() {
+        return (
+            <div className="container">
+                {this.state.posts.map(function (el, index) {
+                    return <RedditPost info={el.data} key={index} />
+                })}
+                <FlatButton onClick={() => this.fetch('reactjs')} label="more" />
+            </div>
+        )
+    }
+}
+export default RedditFeed;
