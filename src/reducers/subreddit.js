a1d9d42f93d98e7c0748582cc5867d13f820a6d0
shadskii <jake.k.hassel@gmail.com>
2018-05-18 03:42:36 -0400

    Fixed spacing

--- src/reducers/subreddit.js
+++ src/reducers/subreddit.js
@@ -4,7 +4,6 @@ export default function subreddit (state = 'reactjs', action) {
     switch (action.type) {
         case types.CHANGE_SUBREDDIT:
             return action.name;
-
         default:
             return state;
     }
