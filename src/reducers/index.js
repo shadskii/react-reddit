e0bff43fe8c67441fe5bb6e830a85919a0d8da5d
shadskii <jake.k.hassel@gmail.com>
2018-05-24 08:38:36 -0400

    Combine reducers into one file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,23 @@
 import { combineReducers } from 'redux';
-import subreddit from './subreddit';
-import view from './view';
+import * as types from '../constants/ActionTypes';
 
+function subreddit (state = 'reactjs', action) {
+    switch (action.type) {
+        case types.CHANGE_SUBREDDIT:
+            return action.name;
+        default:
+            return state;
+    }
+}
+function view (state = false, action) {
+    switch (action.type) {
+        case types.OPEN_MENU:
+            return !state
+        default:
+            return state;
+
+    }
+}
 const rootReducer = combineReducers({
     subreddit,
     view
